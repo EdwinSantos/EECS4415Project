8ce73d3c37659690befaf12404b02d9158b8b44c
Edwin Dos Santos <Edwin.DosSantos@exp.com>
2019-11-15 13:42:55 -0500

    Last commit got messed up because of the merge.

--- userInterface.py
+++ userInterface.py
@@ -29,41 +29,58 @@ teams = {
 
 
 def main():
+
+
+    # Replace this with a list of teams taken from the API
     for team in teams:
         print(repr(team), ":", teams[team])
-    home_team_id = input("Enter home team number: ")
-    away_team_id = input("Enter home team number: ")
-    date = input("Enter date of game (YYYYMMDD): ")
+    home_team_id = int(input("Enter home team number: "))
+    away_team_id = int(input("Enter away team number: "))
     home_team_hashtag = input("Enter home team hashtag: ")
     away_team_hashtag = input("Enter away team hashtag: ")
     neutral_team_hashtag = input("Enter neutral hashtag: ")
 
-    # pass hashtags to other class
-    fixture_ID = get_fixtureID(home_team_id, away_team_id, date)
-    # get_match_events(fixture_ID)
+    # pass hashtags to twitter class that will take care of getting the tweets
+    fixture_ID, timestamp = get_fixtureID(home_team_id, away_team_id)
+    match_events = get_match_events(fixture_ID)
+    # pass events and tweet ratings to class that will make the graph
 
 
-def get_fixtureID(home_team_id, away_team_hashtag, date):
+def get_fixtureID(home_team_id, away_team_id):
     url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/team/" + str(home_team_id) + "/" + str(league_id)
-    # debug code
-
-    #Debug code
-    with open("fixtureID.json") as json_file:
-        json_response = json.load(json_file)
-    #end of debug code
+    
     # Real code
     # response = query_api(url)
     #json_response = json.load(response)
+    
+    #Debug code
+    with open("fixtureID.json", encoding="utf8") as json_file:
+        json_response = json.load(json_file)
 
     for fixture in json_response["api"]["fixtures"]:
-        print(fixture)
-    return 34
+        targetFixture_id = 0
+        if(fixture["homeTeam"]["team_id"] == home_team_id and fixture["awayTeam"]["team_id"] == away_team_id and
+           fixture["league_id"] == 524 and fixture["status"] == "Match Finished"):
+            targetFixture_id = fixture["fixture_id"]
+            timestamp = fixture["event_date"]
+            break
+        # Error catching stuff should go here.
+    return targetFixture_id, timestamp
 
 
-def get_match_events(date):
-    url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524"
-    query_api(url)
-    return
+def get_match_events(fixture_ID):
+    url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/id/" + str(fixture_ID)
+    
+    ##Real code
+    #response = query_api(url)
+    #json_response = json.load(response)
+
+    #Debugging code
+    with open("MatchEvents.json", encoding="utf8") as json_file:
+        json_response = json.load(json_file)
+
+    match_events = json_response["api"]["fixtures"][0]["events"]
+    return match_events
 
 
 def query_api(url):
