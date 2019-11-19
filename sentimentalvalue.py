9363af6c37bf18daf41c1b733d47e5d1cf936c83
ayyounesi <34087369+ayyounesi@users.noreply.github.com>
2019-11-18 23:03:17 -0500

    Update sentimentalvalue.py

--- sentimentalvalue.py
+++ sentimentalvalue.py
@@ -1,14 +1,23 @@
 import re
 import pandas as pd
+import os
+import sys
 from textblob import TextBlob
 from itertools import islice
+from os import listdir
+
+## used for reading multiple csv files in directory
+##CSV_Files = [file for file in listdir('<path to folder>') if file.endswith('.csv')
 
 ## dictionary to hold hashtags and their sentimental value
 dictHashTags = []
 ## parse the dates of tweets
-data = pd.read_csv("tweets.csv", parse_dates=['timestamp'])
+## data = pd.read_csv("tweets.csv", parse_dates=['timestamp'])
+data = pd.read_csv(sys.argv[1], parse_dates=['timestamp'])
 COLS = ['date', 'text', 'sentiment', 'subjectivity', 'polarity']
 df = pd.DataFrame(columns=COLS)
+
+
 
 ## apply sent value on tweets
 for index, row in islice(data.iterrows(), 0, None):
@@ -24,23 +33,17 @@ for index, row in islice(data.iterrows(), 0, None):
     single_survey_sentiment_df = pd.DataFrame([new_entry], columns=COLS)
     df = df.append(single_survey_sentiment_df, ignore_index=True)
 
-##
+## calculate the mean of polarity across all the tweets
+sent_df = df['polarity'].mean()
+file_name =  os.path.basename(sys.argv[1])
+## print hashtag which used for later + polarity value (negative or positive or neutral statement)
+print("Hashtag information and its sentiment value is: " + file_name + " " + str(sent_df), "  ")
+
 df.to_csv('Sentiment_Values.csv', mode='w', columns=COLS, index=False, encoding="utf-8")
-print(df[df.date.dt.strftime('%H:%M:%S').between('06:00', '06:30')])
-##df['date'] = df[df.set_index('date') < '06:30:00'].reset_index()
 
-##print(df)
-
-## needed for later 
+## might use later
 ##hash_tag_retrieval = data.iloc[:, -1]
 ##hash_tag_retrieval = hash_tag_retrieval.to_string()
 ##group = re.findall(r"#(\w+)", hash_tag_retrieval)
 
 ##dictHashTags = dict.fromkeys(group, )
-
-##new_df = data.set_index(['timestamp']).sort_index()
-
-##
-##data = data.reset_index().set_index('timestamp')
-##new_df = data.resample("T")
-
