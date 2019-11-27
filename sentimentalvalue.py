38e577cbed2f517257a2e524c3365f32de1268e6
ayyounesi <34087369+ayyounesi@users.noreply.github.com>
2019-11-27 01:46:40 -0500

    Update sentimentalvalue.py

--- sentimentalvalue.py
+++ sentimentalvalue.py
@@ -6,25 +6,21 @@ from textblob import TextBlob
 from itertools import islice
 from os import listdir
 
-## used for reading multiple csv files in directory
+
 CSV_Files = [file for file in listdir(os.getcwd()) if file.endswith('.csv')]
 dataframe_list = [pd.read_csv(csv_name) for csv_name in CSV_Files]
 print(dataframe_list)
-## dictionary to hold hashtags and their sentimental value
+
 dictHashTags = []
-## parse the dates of tweets
-## data = pd.read_csv("tweets.csv", parse_dates=['timestamp'])
-##data = pd.read_csv(sys.argv[1], parse_dates=['timestamp'])
-##data2 = pd.read_csv(sys.argv[2], parse_dates=['timestamp'])
-##data3 = pd.read_csv(sys.argv[3], parse_dates=['timestamp'])
+
 
 COLS = ['date', 'text', 'sentiment', 'subjectivity', 'polarity']
+COL = ['EndOfTimeWindow', 'Home Team Sentiment', 'Away team', 'Neutral']
 df = pd.DataFrame(columns=COLS)
 df2 = pd.DataFrame(columns=COLS)
 df3 = pd.DataFrame(columns=COLS)
 
-
-## apply sent value on tweets
+## apply sent value on hashtags away
 for index, row in islice(dataframe_list[1].iterrows(), 0, None):
     new_entry = []
     Lower = ((row['text']).lower())
@@ -36,7 +32,8 @@ for index, row in islice(dataframe_list[1].iterrows(), 0, None):
     new_entry += [row['timestamp'], Lower, sentiments, subjectivity, polarity]
     single_survey_sentiment_df = pd.DataFrame([new_entry], columns=COLS)
     df = df.append(single_survey_sentiment_df, ignore_index=True)
-
+    
+## apply sent value on hashtags home
 for index, row in islice(dataframe_list[0].iterrows(), 0, None):
     new_entry2 = []
     Lower2 = ((row['text']).lower())
@@ -49,6 +46,8 @@ for index, row in islice(dataframe_list[0].iterrows(), 0, None):
 
     single_survey_sentiment_df2 = pd.DataFrame([new_entry2], columns=COLS)
     df2 = df2.append(single_survey_sentiment_df2, ignore_index=True)
+    
+## apply sent value on hashtags neutral
 for index, row in islice(dataframe_list[2].iterrows(), 0, None):
     new_entry3 = []
     Lower3 = ((row['text']).lower())
@@ -61,28 +60,43 @@ for index, row in islice(dataframe_list[2].iterrows(), 0, None):
 
     single_survey_sentiment_df3 = pd.DataFrame([new_entry3], columns=COLS)
     df3 = df3.append(single_survey_sentiment_df3, ignore_index=True)
-    
+
 initial_timestamp = df['date'].iloc[0]
 print(initial_timestamp)
-for i in df:
-    
+tmp = 0
+final = pd.DataFrame(columns=COL)
 
+## 
+##for index, row in df2.iterrows():
+   ## if tmp < row['date'] < (initial_timestamp + 300):
+        final = final.append(df2.loc[(df2['date'] >= tmp) & (df2['date'] <= initial_timestamp + 300)])
+        sent_df2 = final['polarity'].mean()
+        print(sent_df2)
+        tmp = row['date']
+        initial_timestamp = tmp
 
-COL = ['EndOfTimeWindow', 'Home Team Sentiment', 'Away team', 'Neutral']
 final_df = pd.DataFrame(columns=COL)
 
 sent_df = df['polarity'].mean()
 sent_df2 = df2['polarity'].mean()
 sent_df3 = df3['polarity'].mean()
 
-new_entry4 = []
-new_entry4 += ['timestamp'+str(+ 300), sent_df, sent_df2, sent_df3]
+##new_entry4 = []
+##new_entry4 += ['timestamp' + str(+ 300), sent_df, sent_df2, sent_df3]
 
 final = pd.DataFrame([new_entry4], columns=COL)
-final_df = final_df.append(final, ignore_index=True)
-print(df)
-final_df.to_csv(r'C:\Users\AmirYounesi\PycharmProjects\bigdata\b\SentimentValues.csv', mode='w', columns=COL, index=False, encoding="utf-8")
-
+##final_df = final_df.append(final, ignore_index=True)
+##print(df)
+##final_df.to_csv(r'C:\Users\AmirYounesi\PycharmProjects\bigdata\b\SentimentValues.csv', mode='w', columns=COL, index=False, encoding="utf-8")
 
 
 ## calculate the mean of polarity across all the tweets
+
+
+## might use later
+##hash_tag_retrieval = data.iloc[:, -1]
+##hash_tag_retrieval = hash_tag_retrieval.to_string()
+##group = re.findall(r"#(\w+)", hash_tag_retrieval)
+
+##dictHashTags = dict.fromkeys(group, )
+
