6becc459721fdebbc925084b41dc6033fb0cc77a
Abdullah Basulaib <basulaib@my.yorku.ca>
2019-12-14 01:42:08 -0500

    more than one game at a time

--- twitter.py
+++ twitter.py
@@ -11,35 +11,20 @@ from tweepy import Stream
 from tweepy import StreamListener
 
 class listener(StreamListener):
-    start_time = time.time()
-    END_TIME = 7200
-    hometag = sys.argv[1]
-    awaytag = sys.argv[2]
-    neutraltag = sys.argv[3]
-    nt = sys.argv[3]
-    stamp = sys.argv[4]
-    if "#" not in hometag:
-        hometag = "#" + hometag
-    if "#" not in awaytag:
-        awaytag = "#" + awaytag
-    if "#" not in neutraltag:
-        neutraltag = "#" + neutraltag
-    if not os.path.exists(nt):
-        os.makedirs(nt)
-    #with open("hashtags.txt", "w") as fs:
-    #    fs.write(hometag + "\n")
-    #    fs.write(awaytag + "\n")
-    #    fs.write(neutraltag + "\n")
-    f = open(nt + "/" + nt + "-" + str(stamp) + ".json", "a", encoding='utf-16')
-    t = time.time()
+    # start time
+    start_time, t = time.time()
+
     def on_data(self, data):
-        if (time.time() - start_time >= 7200):
+        if (time.time() - self.start_time >= END_TIME): # end listener
             return False
         all_data = json.loads(data)
         #if not all_data["lang"] == "en":
         #    return True
+        
+        last_timestamp = 0
         if all_data['retweeted'] or 'RT @' in all_data['text']:
-            return True
+            return True # pass
+
         if "extended_tweet" in all_data:
             if "full_text" in all_data["extended_tweet"]:
                 tweet = all_data["extended_tweet"]["full_text"]
@@ -52,34 +37,46 @@ class listener(StreamListener):
         else:
             tweet = all_data["text"]
             hashtags = all_data["entities"].get("hashtags")
+
         timestamp = all_data["created_at"]
+        timestamp = self.converter(timestamp) # convert tweet timestamp to epoch seconds
+        if windows:
+            timestamp = timestamp - 18000 # unix -> windows epoch conversion
+
+        if last_timestamp == 0:
+            last_timestamp = timestamp
+
         tags = []
         tag = ''
         for dit in hashtags:
             tags.append("#" + dit.get("text"))
-        if hometag in tags:
-            tag = hometag
-        elif awaytag in tags:
-            tag = awaytag
-        elif neutraltag in tags:
-            tag = neutraltag
-        else:
-            tag = ''
+        for t in tags:
+            tmp = self.get_tag(t)
+            if tmp != "":
+                tag = tmp
+                break
         #tags = tags[:-1]
         tweet = re.sub(r'\n', '', tweet)
         tweet = re.sub(r'\t', '', tweet)
-        timestamp = self.converter(timestamp)
 
         string = str(timestamp) + "\t" + tweet + "\t" + tag
+        neutral_tag = self.get_neutral_tag(tag)
+        file_path = neutral_tag + "/" + neutral_tag + "-" + str(last_timestamp) + ".json"
+
         if (time.time() - self.t <= 300):
-            self.f.write(json.dumps(string))
-            self.f.write("\n")
+            with open(file_path, "a+", encoding='utf-16') as fs:
+                fs.write(json.dumps(string))
+                fs.write("\n")
+            # self.f.write(json.dumps(string))
+            # self.f.write("\n")
+
         elif (time.time() - self.t > 300):
             self.t = time.time()
-            self.f.close()
-            self.f = open(nt + "/" + nt + "-" + str(time.time()) + ".json", "a", encoding='utf-16')
-            self.f.write(json.dumps(string))
-            self.f.write("\n")
+            last_timestamp = self.t
+            file_path = neutral_tag + "/" + neutral_tag + "-" + str(last_timestamp) + ".json"
+            with open(file_path, "a+", encoding='utf-16') as fs:
+                fs.write(json.dumps(string))
+                fs.write("\n")
 
         # print(json.dumps(timestamp + "\t" + tweet + "\t" + tags, sort_keys=True, indent=4))
         return True
@@ -94,56 +91,112 @@ class listener(StreamListener):
         epoch = int(time.mktime(time.strptime(epoch, pattern)))
         return epoch
 
-    def new_file(self):
-        string = nt + "/" + nt + "-" + str(time.time()) + ".json"
-        f = open(string, "a", encoding='utf-16')
-        return f
+    def get_tag(self, tt):
+        for tags in ht:
+            if tt in tags:
+                return tt
+            else:
+                return ""
 
-class Twitter(object):
-    ckey = ""
-    csecret = ""
-    atoken = ""
-    asecret = ""
-    hometag = ""
-    awaytag = ""
-    neutraltag = ""
-    nt = ""
-    stamp = 0
-    def __init__(self):
-        with open('twitkeys.txt') as f:
-            ckey = f.readline().rstrip()
-            csecret = f.readline().rstrip()
-            atoken = f.readline().rstrip()
-            asecret = f.readline().rstrip()
-        hometag = sys.argv[1]
-        awaytag = sys.argv[2]
-        neutraltag = sys.argv[3]
-        nt = sys.argv[3]
-        stamp = sys.argv[4]
-        if "#" not in hometag:
-            hometag = "#" + hometag
-        if "#" not in awaytag:
-            awaytag = "#" + awaytag
-        if "#" not in neutraltag:
-            neutraltag = "#" + neutraltag
-    def main(self):
-        auth = OAuthHandler(ckey, csecret)
-        auth.set_access_token(atoken, asecret)
-        twitterStream = Stream(auth, listener(), tweet_mode='extended')
-        twitterStream.filter(track=[hometag,awaytag,neutraltag],encoding='utf-8',languages=["en"])
-        twitterStream.running = False
-        if not twitterStream.running:
-            print("stream finished")
+    def get_neutral_tag(self, tg):
+        for tags in ht:
+            if tg in tags:
+                return tags[2]
+            else:
+                return ""
 
+# end of game in seconds
+END_TIME = 7200
 
+# Windows epoch is 18000 seconds behind Unix
+windows = True if "win" in sys.platform else False
 
+# format: [[Hometag, Awaytag, Neutraltag, Timestamp, ...], [...]]
+matches = json.loads(sys.argv[1])
+tracking = []
+ht = []
+neutral_tags = []
+# make list of hashtags (i.e., remove timestamp + other unwanted information) format: [[home, away, neutral], [...]]
+for game in matches:
+    ls = []
+    if "#" not in game[0]:
+        ls.append("#" + game[0])
+    else:
+        ls.append(game[0])
+    if "#" not in game[1]:
+        ls.append("#" + game[1])
+    else:
+        ls.append(game[1])
+    if "#" not in game[2]:
+        ls.append("#" + game[2])
+    else:
+        ls.append(game[2])
+    ht.append(ls)
 
-#def main():
-    # client = Twitter()
-    # client.get_tweets(query='#warriors -filter:retweets -filter:links',count=100)
-    #stream = tweepy.Stream(auth = client.api.auth)
-    #for t in tweets[:10]:
-     #   print(t + "\n")
+for lst in ht:
+    tracking.append(lst[0])
+    tracking.append(lst[1])
+    tracking.append(lst[2])
 
-# if __name__=="__main__":
-    # main()
+# make list of [[neutrals, timestamp], [...]]
+i = 2
+while i <= len(matches):
+    neutral_tags.append([ht[i], ht[i + 1]])
+    i += 3
+
+# write hashtags to file
+with open("hashtags.txt", "w+") as fs:
+    for tag in ht:
+        fs.write(tag + "\n")
+
+# write neutral hashtags to file
+with open("nt_hashtags.txt", "w+") as fs:
+    for tag in neutral_tags:
+        fs.write(tag + "\n")
+
+# make neutrals directory
+for tag in neutral_tags:
+    if not os.path.exists(tag[0]):
+        os.makedirs(tag[0])
+
+# class Twitter(object):
+#     ckey = ""
+#     csecret = ""
+#     atoken = ""
+#     asecret = ""
+#     hometag = ""
+#     awaytag = ""
+#     neutraltag = ""
+#     nt = ""
+#     stamp = 0
+#     def __init__(self):
+#         with open('twitkeys.txt') as f:
+#             ckey = f.readline().rstrip()
+#             csecret = f.readline().rstrip()
+#             atoken = f.readline().rstrip()
+#             asecret = f.readline().rstrip()
+#         hometag = sys.argv[1]
+#         awaytag = sys.argv[2]
+#         neutraltag = sys.argv[3]
+#         nt = sys.argv[3]
+#         stamp = sys.argv[4]
+#         if "#" not in hometag:
+#             hometag = "#" + hometag
+#         if "#" not in awaytag:
+#             awaytag = "#" + awaytag
+#         if "#" not in neutraltag:
+#             neutraltag = "#" + neutraltag
+
+with open('twitkeys.txt') as f:
+    ckey = f.readline().rstrip()
+    csecret = f.readline().rstrip()
+    atoken = f.readline().rstrip()
+    asecret = f.readline().rstrip()
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+twitterStream = Stream(auth, listener(), tweet_mode='extended')
+twitterStream.filter(track=tracking,encoding='utf-8',languages=["en"])
+twitterStream.running = False
+if not twitterStream.running:
+    print("stream finished")
