de2b1170581fb11a3520768c67bad260db2920f3
Abdullah Basulaib <basulaib@my.yorku.ca>
2019-12-15 14:12:28 -0500

    twitter app done, added 3 more games

--- twitter.py
+++ twitter.py
@@ -11,13 +11,14 @@ from tweepy import Stream
 from tweepy import StreamListener
 
 class listener(StreamListener):
-    start_time = time.time()
+    start_time = time.time() # start of the farmer
     t = time.time()
     last_timestamp = 0
 
     def on_data(self, data):
-        if (time.time() - self.start_time >= END_TIME): # end listener
+        if (time.time() - self.start_time >= END_TIME): # end of the game + extra stoppage time
             return False
+
         all_data = json.loads(data)
         if all_data['retweeted'] or 'RT @' in all_data['text']:
             return True # pass any retweets
@@ -38,7 +39,7 @@ class listener(StreamListener):
         timestamp = all_data["created_at"]
         timestamp = self.converter(timestamp) # convert tweet timestamp to epoch seconds
         if windows:
-            timestamp = timestamp - 18000 # unix -> windows epoch conversion
+            timestamp = timestamp - 18000 # windows -> unix epoch conversion
 
         if self.last_timestamp == 0:
             self.last_timestamp = self.t
@@ -48,27 +49,26 @@ class listener(StreamListener):
         for dit in hashtags:
             tags.append("#" + dit.get("text"))
         for t in tags:
-            tmp = self.get_tag(t)
+            tmp = self.get_tag(t) # find the first relevant hashtag (HOME, AWAY, or NEUTRAL)
             if tmp != "":
                 tag = tmp
                 break
-        #tags = tags[:-1]
         tweet = re.sub(r'\n', '', tweet)
         tweet = re.sub(r'\t', '', tweet)
 
-        string = str(timestamp) + "\t" + tweet + "\t" + tag
-        neutral_tag = self.get_neutral_tag(tag)
+        string = str(timestamp) + "\t" + tweet + "\t" + tag # construct formatted tweet
+        neutral_tag = self.get_neutral_tag(tag) # find which game the hashtag we found from the tweet belongs to
         if neutral_tag == "":
             return True
         file_path = neutral_tag + "/" + neutral_tag + "-" + str(self.last_timestamp) + ".json"
 
+        # if under 5 minute intervals, keep writing to the same file
         if (time.time() - self.t <= 300):
             with open(file_path, "a+", encoding='utf-16') as fs:
                 fs.write(json.dumps(string))
                 fs.write("\n")
-            # self.f.write(json.dumps(string))
-            # self.f.write("\n")
 
+        # else write a new file
         elif (time.time() - self.t > 300):
             self.t = time.time()
             self.last_timestamp = self.t
@@ -77,19 +77,20 @@ class listener(StreamListener):
                 fs.write(json.dumps(string))
                 fs.write("\n")
 
-        # print(json.dumps(timestamp + "\t" + tweet + "\t" + tags, sort_keys=True, indent=4))
         return True
 
     def on_error(self, status):
         # print (status)
         return
 
+    # convert tweet timestamp to Epoch  (e.g. 'Tue Mar 29 08:11:25 +0000 2010' to Epoch)
     def converter(self, ts):
         epoch = time.strftime('%d-%m-%Y %H:%M:%S', time.strptime(ts,'%a %b %d %H:%M:%S +0000 %Y'))
         pattern = '%d-%m-%Y %H:%M:%S'
         epoch = int(time.mktime(time.strptime(epoch, pattern)))
         return epoch
 
+    # find relevant tag from tweet
     def get_tag(self, tt):
         result = ""
         for tags in ht:
@@ -97,6 +98,7 @@ class listener(StreamListener):
                 result = tt
         return result
 
+    # find which game the hashtag belongs to
     def get_neutral_tag(self, tg):
         result = ""
         for tags in ht:
@@ -107,15 +109,15 @@ class listener(StreamListener):
 # end of game in seconds
 END_TIME = 7200
 
-# Windows epoch is 18000 seconds behind Unix
+# Windows epoch is 18000 seconds (5 hours) ahead Unix
 windows = True if "win" in sys.platform else False
 
 # format: [[Hometag, Awaytag, Neutraltag, Timestamp, ...], [...]]
-print(sys.argv[1])
 matches = json.loads(sys.argv[1])
 tracking = []
 ht = []
 neutral_tags = []
+
 # make list of hashtags (i.e., remove timestamp + other unwanted information) format: [[home, away, neutral], [...]]
 for game in matches:
     ls = []
@@ -133,75 +135,34 @@ for game in matches:
         ls.append(game[2])
     ht.append(ls)
 
+# make list of hashtags to track [home_1, away_1, neutral_1, home_2, away_2, neutral_2, ...]
 for lst in ht:
     tracking.append(lst[0])
     tracking.append(lst[1])
     tracking.append(lst[2])
 
-# make list of [[neutrals, timestamp], [...]]
+# make list of neutral hashtags[neutral_1, neutral_2, ...]
 for t in ht:
     neutral_tags.append(t[2])
 
-print(neutral_tags)
-
-# i = 1
-# while i <= len(matches):
-#     neutral_tags.append([ht[i], ht[i + 1]])
-#     i += 3
-
-# write hashtags to file
-#with open("hashtags.txt", "w+") as fs:
-#    for tag in ht:
-#        fs.write(tag + "\n")
-
-# write neutral hashtags to file
-#with open("nt_hashtags.txt", "w+") as fs:
-#    for tag in neutral_tags:
-#        fs.write(tag + "\n")
-
-# make neutrals directory
+# make neutrals directories
 for tag in neutral_tags:
     if not os.path.exists(tag):
         os.makedirs(tag)
 
-# class Twitter(object):
-#     ckey = ""
-#     csecret = ""
-#     atoken = ""
-#     asecret = ""
-#     hometag = ""
-#     awaytag = ""
-#     neutraltag = ""
-#     nt = ""
-#     stamp = 0
-#     def __init__(self):
-#         with open('twitkeys.txt') as f:
-#             ckey = f.readline().rstrip()
-#             csecret = f.readline().rstrip()
-#             atoken = f.readline().rstrip()
-#             asecret = f.readline().rstrip()
-#         hometag = sys.argv[1]
-#         awaytag = sys.argv[2]
-#         neutraltag = sys.argv[3]
-#         nt = sys.argv[3]
-#         stamp = sys.argv[4]
-#         if "#" not in hometag:
-#             hometag = "#" + hometag
-#         if "#" not in awaytag:
-#             awaytag = "#" + awaytag
-#         if "#" not in neutraltag:
-#             neutraltag = "#" + neutraltag
-
+# read twitter keys
 with open('twitkeys.txt') as f:
     ckey = f.readline().rstrip()
     csecret = f.readline().rstrip()
     atoken = f.readline().rstrip()
     asecret = f.readline().rstrip()
 
+# initialize the listener
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 twitterStream = Stream(auth, listener(), tweet_mode='extended')
+print("Twitter Listener started...")
 twitterStream.filter(track=tracking,encoding='utf-8',languages=["en"])
 twitterStream.running = False
 if not twitterStream.running:
-    print("stream finished")
+    print("Twitter Listener finished")
