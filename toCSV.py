f0f44ac61e5d5d6f1d70a7c9ba2c50d8915b95f4
Paul Kim <32376163+pyjaekim@users.noreply.github.com>
2019-12-14 19:21:15 -0500

    clean code

--- toCSV.py
+++ toCSV.py
@@ -27,8 +27,6 @@ for game in matches:
         ls.append(game[2])
     ht.append(ls)
 
-# matchlist =   [["SUFC", "AVFC", "SUFAVL"], ["LCFC", "NCFC", "LeiNor"]]
-
 # Iterate through each match folder, reading all files and organizing them by hashtags
 for match in ht:
     os.chdir(match[2])
@@ -49,64 +47,15 @@ for match in ht:
                         line = re.split(r'\\t', line)
                         writer.writerow(line)
 
+    # Read csv as dataframes split between the hashtags, and create a separate csv 
+    # for each hashtag
     df = pd.read_csv('tweets.csv')
     home = df[['timestamp','text','hashtags']].loc[df['hashtags'] == match[0]]
     neutral = df[['timestamp','text','hashtags']].loc[df['hashtags'] == match[2]]
     away = df[['timestamp','text','hashtags']].loc[df['hashtags'] == match[1]]
-
-    # export_csv1 = home.to_csv('home.csv', index=False)
-    # export_csv2 = neutral.to_csv('neutral.csv', index=False)
-    # export_csv3 = away.to_csv('away.csv', index=False)
 
     export_csv1 = home.to_csv(match[0] + '.csv', index=False)
     export_csv2 = neutral.to_csv(match[2] + '.csv', index=False)
     export_csv3 = away.to_csv(match[1] + '.csv', index=False)
 
     os.chdir('..')
-
-
-
-
-
-    # export_csv1 = home.to_csv(r'C:\Users\Paul\Desktop\home.csv', index=False)
-    # export_csv2 = neutral.to_csv(r'C:\Users\Paul\Desktop\neutral.csv', index=False)
-    # export_csv3 = away.to_csv(r'C:\Users\Paul\Desktop\away.csv', index=False)
-
-# with open('tweets.csv', 'r+', newline ='') as readcsv:
-#     spamreader = csv.DictReader(readcsv)
-#     # sortedlist = sorted(spamreader, key=lambda row: row[2], reverse=False )
-#     print(spamreader)
-
-
-# with open('sortedtweets.csv', 'w+') as sortedfile:
-#     fieldnames = ['timestamp', 'text', 'hashtags']
-#     # writer = csv.DictWriter(sortedfile, lineterminator = '\n')
-#     # writer.writerow(('timestamp', 'text', 'hashtags'))
-#     sortwriter = csv.DictWriter(sortedfile, fieldnames=fieldnames)
-#     sortwriter.writeheader()
-#     for row in sortedlist:
-#         writer.writerow(row)
-
-
-# # format: [[Hometag, Awaytag, Neutraltag, Timestamp, ...], [...]]
-# print(sys.argv[1])
-# matches = json.loads(sys.argv[1])
-# tracking = []
-# ht = []
-# neutral_tags = []
-# # make list of hashtags (i.e., remove timestamp + other unwanted information) format: [[home, away, neutral], [...]]
-# for game in matches:
-#     ls = []
-#     if "#" not in game[0]:
-#         ls.append("#" + game[0])
-#     else:
-#         ls.append(game[0])
-#     if "#" not in game[1]:
-#         ls.append("#" + game[1])
-#     else:
-#         ls.append(game[1])
-#     if "#" not in game[2]:
-#         ls.append("#" + game[2])
-#     else:
-#         ls.append(game[2])
-#     ht.append(ls)
