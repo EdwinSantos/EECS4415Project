e3be2acc51942a129eaf0cf32fb2869495f7c6ed
Paul Kim <32376163+pyjaekim@users.noreply.github.com>
2019-12-14 19:01:22 -0500

    csv fix

--- toCSV.py
+++ toCSV.py
@@ -8,10 +8,29 @@ import json
 import pandas as pd
 from contextlib import suppress
 
-matchlist =   [["SUFC", "AVFC", "SUFAVL"], ["LCFC", "NCFC", "LeiNor"]]
+matches = json.loads(sys.argv[1])
+ht = []
+# make list of hashtags (i.e., remove timestamp + other unwanted information) format: [[home, away, neutral], [...]]
+for game in matches:
+    ls = []
+    if "#" not in game[0]:
+        ls.append("#" + game[0])
+    else:
+        ls.append(game[0])
+    if "#" not in game[1]:
+        ls.append("#" + game[1])
+    else:
+        ls.append(game[1])
+    if "#" not in game[2]:
+        ls.append("#" + game[2])
+    else:
+        ls.append(game[2])
+    ht.append(ls)
+
+# matchlist =   [["SUFC", "AVFC", "SUFAVL"], ["LCFC", "NCFC", "LeiNor"]]
 
 # Iterate through each match folder, reading all files and organizing them by hashtags
-for match in matchlist:
+for match in ht:
     os.chdir('#' + match[2])
 
     with suppress(Exception):
@@ -22,7 +41,6 @@ for match in matchlist:
             for filename in os.listdir('.'):
                 with open(filename, 'r', encoding='utf-16' ) as txtfile:
                     lines = txtfile.readlines()
-                    # lines = json.load(txtfile)  
 
                     for line in lines:
                         line = line.strip()
@@ -36,9 +54,13 @@ for match in matchlist:
     neutral = df[['timestamp','text','hashtags']].loc[df['hashtags'] == '#' + match[2]]
     away = df[['timestamp','text','hashtags']].loc[df['hashtags'] == '#' + match[1]]
 
-    export_csv1 = home.to_csv('home.csv', index=False)
-    export_csv2 = neutral.to_csv('neutral.csv', index=False)
-    export_csv3 = away.to_csv('away.csv', index=False)
+    # export_csv1 = home.to_csv('home.csv', index=False)
+    # export_csv2 = neutral.to_csv('neutral.csv', index=False)
+    # export_csv3 = away.to_csv('away.csv', index=False)
+
+    export_csv1 = home.to_csv('#' + match[0] + '.csv', index=False)
+    export_csv2 = neutral.to_csv('#' + match[2] + '.csv', index=False)
+    export_csv3 = away.to_csv('#' + match[1] + '.csv', index=False)
 
     os.chdir('..')
 
